c39df9e32e3567e43415a26dc40e919c250499fd
ckrdi <agung.cokorda@gmail.com>
2021-03-15 12:02:06 +0800

    Added chart.js lib and made the chart appear on page.

--- script.js
+++ script.js
@@ -1,14 +1,51 @@
+// data from : https://data.giss.nasa.gov/gistemp/
+// mean from : https://earthobservatory.nasa.gov/world-of-change/DecadalTemp
+
 getData();
 
 async function getData() {
   const response = await fetch('data/ZonAnn.Ts+dSST.csv');
   const data = await response.text();
-
+  const xs = [];
+  const ys = [];
   const row = data.split('\n').slice(1);
-  for(let i = 0; i < row.length; i++) {
+  for (let i = 0; i < row.length; i++) {
     const columns = row[i].split(',');
     const year = columns[0];
     const temp = columns[1];
-    console.log(year, temp);
+    xs.push(year);
+    ys.push(parseFloat(temp) + 14);
   }
+
+  const ctx = document.getElementById('myChart').getContext('2d');
+  const chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+      labels: xs,
+      datasets: [{
+        label: 'Global Annual Mean Surface Air Temperature Change in C°',
+        fill: false,
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: ys
+      }]
+    },
+
+    // Configuration options go here
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            // Include a sign in the ticks
+            callback: function (value, index, values) {
+              return value + "°";
+            }
+          }
+        }]
+      }
+    }
+  });
 }
